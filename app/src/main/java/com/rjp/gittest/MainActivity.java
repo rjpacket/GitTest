75a4214268f36a6c3c41a3d2d44f9adf1632f8e4
jinpeng.ren <jinpeng.ren@11bee.com>
2019-05-20 14:26:45 +0800

    dev 1111111111111111

--- app/src/main/java/com/rjp/gittest/MainActivity.java
+++ app/src/main/java/com/rjp/gittest/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
         System.out.println("master 2222222222222222222222");
         System.out.println("master 3333333333333333333333");
+        System.out.println("dev 111111111111111");
 
     }
 }
