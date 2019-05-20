4373e8c641fef397b94ea36eb301aa8d7419e9ce
jinpeng.ren <jinpeng.ren@11bee.com>
2019-05-20 14:11:15 +0800

    33333333333333333333333333

--- app/src/main/java/com/rjp/gittest/MainActivity.java
+++ app/src/main/java/com/rjp/gittest/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
         System.out.println("rjp 的第一次提交");
         System.out.println("rjp 的第二次提交");
+        System.out.println("rjp 的第三次提交");
         System.out.println("rjp 的第五次提交");
     }
 }
