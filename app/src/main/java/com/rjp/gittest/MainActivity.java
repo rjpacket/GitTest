14b6db0c53c571368155b25aefb9610919e1f3ed
jinpeng.ren <jinpeng.ren@11bee.com>
2019-05-21 19:53:26 +0800

    修改了一些内容

--- app/src/main/java/com/rjp/gittest/MainActivity.java
+++ app/src/main/java/com/rjp/gittest/MainActivity.java
@@ -10,11 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("master 2222222222222222222222");
-        System.out.println("master 3333333333333333333333");
-        System.out.println("dev 111111111111111");
-        System.out.println("dev 4444444444444444444");
-        System.out.println("dev 5555555555555555555");
 
     }
 }
